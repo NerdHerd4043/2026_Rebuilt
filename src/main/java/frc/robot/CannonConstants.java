b5c1f2f36c0b80ce2e4167da53dd21e99c9e7cb9
Cade Waters <steemponk@gmail.com>
2026-01-17 13:58:47 -0800

    updating constants and adding skeleton

---
+++ src/main/java/frc/robot/CannonConstants.java
@@ -0,0 +1,57 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot;
+
+import edu.wpi.first.math.geometry.Translation2d;
+import edu.wpi.first.math.util.Units;
+import edu.wpi.first.math.system.plant.DCMotor;
+
+/**
+ * The Constants class provides a convenient place for teams to hold robot-wide
+ * numerical or boolean
+ * constants. This class should not be used for any other purpose. All constants
+ * should be declared
+ * globally (i.e. public static). Do not put anything functional in this class.
+ *
+ * <p>
+ * It is advised to statically import this class (or one of its inner classes)
+ * wherever the
+ * constants are needed, to reduce verbosity.
+ */
+public final class CannonConstants {
+  
+  public static final class ArmConstants {
+    //Motor IDs
+    public static final int leftArmMotorID = 5;
+
+    //PID values
+    public static final class PIDValues {
+      public static final double p = 4;
+      public static final double i = 0;
+      public static final double d = 1.7;
+    }
+
+    public static final class FeedForwardValues {
+      public static final double kS = 0.07;
+      public static final double kG = 0.4;
+      public static final double kV = 3;
+    }
+
+    public static final class ArmPositions {
+      public static final double upPos = 0.5;
+      public static final double downPos = 1.5;
+    }
+  }
+
+  public static final class IntakeConstants {
+    public static final int intakeMotorID = 7;
+    public static final int kickupMotorID = 17;
+    public static final double intakeSpeed = 0.7;
+    public static final double kickupSpeed = 0.7;
+
+    public static final int currentLimit = 30;
+  }
+
+}
---
+++ src/main/java/frc/robot/Constants.java
@@ -0,0 +1,61 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot;
+
+import edu.wpi.first.math.geometry.Translation2d;
+import edu.wpi.first.math.util.Units;
+import edu.wpi.first.math.system.plant.DCMotor;
+
+/**
+ * The Constants class provides a convenient place for teams to hold robot-wide
+ * numerical or boolean
+ * constants. This class should not be used for any other purpose. All constants
+ * should be declared
+ * globally (i.e. public static). Do not put anything functional in this class.
+ *
+ * <p>
+ * It is advised to statically import this class (or one of its inner classes)
+ * wherever the
+ * constants are needed, to reduce verbosity.
+ */
+public final class Constants {
+  public static final class DriveConstants {
+    public static final double deadband = 0.08;
+    public static final int currentLimit = 40;
+    public static final double slewRate = 20; // lower number for higher center of mass
+    public static final int temp = 21;
+    public static final double DRIVE_REDUCTION = 1.0 / 6.75;
+    public static final double NEO_FREE_SPEED = 5820.0 / 60.0;
+    public static final double WHEEL_DIAMETER = 0.1016;
+    public static final double MAX_VELOCITY = NEO_FREE_SPEED * DRIVE_REDUCTION * WHEEL_DIAMETER * Math.PI;
+    public static final double MAX_ANGULAR_VELOCITY = MAX_VELOCITY / (ModuleLocations.dist / Math.sqrt(2.0));
+
+    public static final class SwervePID {
+      public static final double p = 0.12;
+      public static final double i = 0;
+      public static final double d = 0.0015;
+    }
+
+    public static final class SwerveModules {
+
+    }
+
+    public static final class ModuleLocations {
+      public static final double dist = Units.inchesToMeters(9.25);
+      public static final double robotRaduius = Math.sqrt(2 * Math.pow(dist, 2));
+      public static final Translation2d frontLeft = new Translation2d(dist, dist);
+      public static final Translation2d frontRight = new Translation2d(dist, -dist);
+      public static final Translation2d backLeft = new Translation2d(-dist, dist);
+      public static final Translation2d backRight = new Translation2d(-dist, -dist);
+    }
+  }
+
+  public static final class RobotConfigInfo {
+
+  
+
+  }
+
+}
