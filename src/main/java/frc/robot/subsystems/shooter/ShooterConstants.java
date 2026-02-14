75271e33ad9d60e95d1c4398806dd1404f3d83fa
Michael2121212121 <michaeldillman25@gmail.com>
2026-02-13 19:59:10 -0800

    shooter pid

--- src/main/java/frc/robot/subsystems/shooter/ShooterConstants.java
+++ src/main/java/frc/robot/subsystems/shooter/ShooterConstants.java
@@ -5,4 +5,14 @@ public class ShooterConstants {
     public static final int indexerMotorID = 32;
     public static final double flyWheelMaxSpeed = 1.0;
     public static final double indexerFeedSpeed = 0.25;
+
+    public static final class FlyWheelPID {
+        public static final double p = 0;
+        public static final double i = 0;
+        public static final double d = 0;
+        public static final double Iz = 0;
+        public static final double ff = 0;
+        public static final double maxOutput = 0;
+        public static final double minOutput = 0;
+  }
 }
