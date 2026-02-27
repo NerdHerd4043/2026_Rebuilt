4eb92310a2eac11739676547a143d97e836cbd27
Zynh Ludwig <zynh0722@gmail.com>
2026-02-26 18:59:01 -0800

    SparkBaseLogger.java: init

---
+++ src/main/java/cowlib/logging/SparkBaseLogger.java
@@ -0,0 +1,26 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package cowlib.logging;
+
+import com.revrobotics.spark.SparkBase;
+
+import edu.wpi.first.epilogue.CustomLoggerFor;
+import edu.wpi.first.epilogue.logging.ClassSpecificLogger;
+import edu.wpi.first.epilogue.logging.EpilogueBackend;
+
+/** Add your docs here. */
+@CustomLoggerFor(SparkBase.class)
+public class SparkBaseLogger extends ClassSpecificLogger<SparkBase> {
+  public SparkBaseLogger() {
+    super(SparkBase.class);
+  }
+
+  @Override
+  protected void update(EpilogueBackend backend, SparkBase motor) {
+    backend.log("Applied Output (Duty Cycle)", motor.getAppliedOutput());
+    backend.log("Output Current (A)", motor.getOutputCurrent());
+    backend.log("Temperature (C)", motor.getMotorTemperature());
+  }
+}
