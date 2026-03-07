c03b87976642105b37b14cbdbac11677907c6a73
Michael2121212121 <michaeldillman25@gmail.com>
2026-03-07 12:55:48 -0800

    Update at 'Sat Mar 07 12:55:48 PST 2026'

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -210,6 +210,7 @@ public class RobotContainer {
 
     c_driveStick.b().whileTrue(intake.reveseIntakeCommand());
     c_driveStick.b().whileFalse(intake.stopReversingIntake());
+    c_driveStick.b().whileTrue(shooter.reverseIndexer());
 
     // right trigger runs the intake
     c_driveStick.leftBumper().whileTrue(intake.intakeCommand);
