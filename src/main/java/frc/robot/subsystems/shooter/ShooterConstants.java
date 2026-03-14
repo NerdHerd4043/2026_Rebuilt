41b62399936b99919840d7933618746ab22711bd
Michael2121212121 <michaeldillman25@gmail.com>
2026-03-14 12:55:08 -0700

    Update at 'Sat Mar 14 12:55:07 PDT 2026'

--- src/main/java/frc/robot/subsystems/shooter/ShooterConstants.java
+++ src/main/java/frc/robot/subsystems/shooter/ShooterConstants.java
@@ -14,7 +14,7 @@ public class ShooterConstants {
   public static final class FlyWheelPID {
     public static final double p = 0.0001;
     public static final double i = 0.00000001;
-    public static final double d = 0;
+    public static final double d = 0.00000001;
 
     public static final double maxOutput = 1;
     public static final double minOutput = -1;
